de11e5348a3edb1e21ae6d9b87010e87d9ca9216
Dillon Hess <dhess268@gmail.com>
2022-04-04 22:14:11 -0400

    add another solution

--- solutions.js
+++ solutions.js
@@ -11,3 +11,56 @@ const rps = (p1, p2) => {
       return 'Player 2 won!'
     }
   };
+
+
+// Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n
+
+// I didnt fully understand the rules of this problem so I had troubles figuring it out
+  function eachCons(array, n) {
+    //   let total = []
+    //  for(let i = 0; i < array.length - n + 1; i++){
+    //     let innertotal = []
+    //     total.push(innertotal)
+    //     for(let j = 0; j < Math.ceil(array/n); j++){
+    //       total[i].push(j)
+    //     }
+    //   }
+      
+    //   return total
+      let total = []
+      if(array.length === 0){
+        return []
+      }
+      for(let i = 0; i + n - 1< array.length; i+= 1){
+        let innertotal = []
+        
+          for(let j = i; j < i + n; j++){
+            innertotal.push(array[j])
+          }
+        
+        total.push(innertotal)
+      }
+      
+      
+    //   for(let i = 0; i < array.length + n - 1; i+= n){
+    //     console.log(i, (array.length - n + 1))
+    //     if(i + n >= array.length){
+    //       let innerarray = []
+          
+    //       for(let j = array.length - n; j < array.length; j++){
+    //         innerarray.push(array[j])
+    //       }
+    //       total.push(innerarray)
+    //     }
+    //     else{
+    //       let innerarray = []
+    //       for(let j = i; j < n + i; j++){
+    //         innerarray.push(array[j])
+    //       }
+    //       total.push(innerarray)
+    //     }
+    //   }
+      
+      
+      return total
+    }
